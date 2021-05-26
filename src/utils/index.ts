fcfc7a641b12546b6a533f089c9d0de5f1cf15c2
Eduardo Diaz <eduardo.diaz.268@gmail.com>
2021-05-26 01:27:24 -0500

    feat: function findByCode

---
+++ src/utils/index.ts
@@ -0,0 +1,20 @@
+import { IUbigeo } from '../types';
+
+const findByCode = (
+  code: string,
+  map: Map<string, IUbigeo>
+): Array<IUbigeo | undefined> | undefined => {
+  let i = 0;
+  let uid;
+  const arr: Array<IUbigeo | undefined> = [];
+
+  do {
+    i += 1;
+    uid = `${code}${String(i).padStart(2, '0')}`;
+    if (map.has(uid)) arr.push(map.get(uid));
+  } while (map.has(uid));
+
+  return arr;
+};
+
+export { findByCode };
